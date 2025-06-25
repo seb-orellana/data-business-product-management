072551949d540a8ef17934bd9f1bc3afae4acf36
Sebastian Orellana Moreno <sebastian.orellana.eng@gmail.com>
2025-06-24 20:41:05 -0500

    Changed path using os to pathlib.

--- v2-upgrade/config.py
+++ v2-upgrade/config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,5 +7,5 @@ load_dotenv()
 admin_user = os.getenv("ADMIN_USERNAME", "admin")
 admin_pass = os.getenv("ADMIN_PASSWORD", "changeme")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.getenv("DB_PATH", os.path.join(BASE_DIR, "Database", "store.db"))
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = os.getenv("DB_PATH", str(BASE_DIR / "Database" / "store.db"))
